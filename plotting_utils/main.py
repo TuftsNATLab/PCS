033cd24efe6f07006324159ad3a2f2e79b3c69d3
abdullah <abdullah@cs.tufts.edu>
2024-06-04 21:25:58 -0400

    changed trace numbers

--- plotting_utils/main.py
+++ plotting_utils/main.py
@@ -5,11 +5,12 @@ import plotting as pl
 def main():
     # To only produce certain plots -- feel free to comment out 
     # subset of these function calls here
-    pl.plot_fig1a()
+    # pl.plot_fig1a()
+    # pl.plot_fig1b()
     # pl.plot_fig4()
     # pl.plot_fig5()
     # pl.plot_fig6()
-    # pl.plot_fig7()
+    pl.plot_fig7()
     # pl.plot_fig8()
     # pl.plot_fig9()
     # pl.plot_fig10()
